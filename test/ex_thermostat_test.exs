743604d7f3d759cf972ac24c9ac6bf451dc4a454
Hez Ronningen <code@hez.ca>
2025-03-09 09:26:36 -0700

    fix tests

--- test/ex_thermostat_test.exs
+++ test/ex_thermostat_test.exs
@@ -3,15 +3,13 @@ defmodule ExThermostatTest do
   doctest ExThermostat
 
   setup do
-    pub_sub = Phoenix.PubSub.start_link(name: ExThermostat.PubSub)
     ex_thermostat = ExThermostat.start_link()
-    {:ok, ex_thermostat: ex_thermostat, pub_sub: pub_sub}
+    {:ok, ex_thermostat: ex_thermostat}
   end
 
   describe "status/0" do
     test "returns the status", %{ex_thermostat: ex_thermostat} do
       status = ExThermostat.status()
-      dbg(status)
     end
   end
 end
